a93bc2e7c2bcf60070ceb5c948f25f47257351eb
Denis <denis.g@all4stream.com>
2022-07-26 14:31:06 +0300

    .

--- server/server.go
+++ server/server.go
@@ -18,36 +18,11 @@ type GRPCServer struct { // implements definition from Proto
 func GrpcServer() {
 
 	var (
-		//err      error
-		//listener net.Listener
 		logg = logger.Log.WithFields(log.Fields{
 			"module": "grpc_server",
 		})
+		wg sync.WaitGroup
 	)
-
-	//logg.Infof("start grpc server on port: %s", config.Params.GRPCPort)
-	//
-	//opts := []grpc.ServerOption{
-	//	grpc.KeepaliveParams(keepalive.ServerParameters{
-	//		MaxConnectionAge: 10 * time.Second,
-	//	}),
-	//}
-	//
-	//gs := grpc.NewServer(opts...)
-	//
-	//reflection.Register(gs)
-	//RegisterBalancerServer(gs, &GRPCServer{})
-	//
-	//listener, err = net.Listen("tcp", ":"+config.Params.GRPCPort)
-	//if err != nil {
-	//	logg.Fatalf("error on create listener for grpc server: %v", err)
-	//}
-	//
-	//if err = gs.Serve(listener); err != nil {
-	//	logg.Fatalf("error on create grpc server: %v", err)
-	//}
-
-	var wg sync.WaitGroup
 
 	for _, addr := range config.Params.GRPCPorts {
 
